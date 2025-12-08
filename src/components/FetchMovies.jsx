94783ba43536b8ef40bf1339750142c3338eabdf
Virgil I Ambar <imanuelambar11@gmail.com>
2025-12-08 16:46:55 +0700

    feat: edit apikey and use from file .env

--- src/components/FetchMovies.jsx
+++ src/components/FetchMovies.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 // https://image.tmdb.org/t/p/w500/
 const FetchMovies = () => {
   const [allTmdb, setAllTmdb] = useState([]);
   const [genres, setGenres] = useState([]);
 
+  const apiKeyMovie = import.meta.env.VITE_TMDB_API_KEY_MOVIE;
+  const apikeyGenre = import.meta.env.VITE_TMDB_API_KEY_GENRE;
+  //
   useEffect(() => {
     (async function () {
       try {
-        const tmdbUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=28b976ea87c130e70edaeb0ff3cf756a`;
-        const tmdbGenre =
-          "https://api.themoviedb.org/3/genre/movie/list?api_key=28b976ea87c130e70edaeb0ff3cf756a";
+        const tmdbUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKeyMovie}`;
+        const tmdbGenre = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apikeyGenre}`;
         const response1 = await fetch(tmdbUrl);
         //
         if (!response1.ok) {
@@ -21,6 +24,8 @@ const FetchMovies = () => {
 
         const response2 = await fetch(tmdbGenre);
         const datas2 = await response2.json();
+
+        console.log(datas2);
 
         setGenres(datas2.genres);
 
@@ -33,16 +38,16 @@ const FetchMovies = () => {
   }, []);
 
   // ambil nama genre dari genre_ids
-  const getMovieGenres = (genreIds) => {
+  const getMovieGenres = (genre_Ids) => {
     return genres.filter((genre) => {
-      return genreIds.includes(genre.id);
+      return genre_Ids.includes(genre.id);
     });
   };
 
   return (
     <section className="flex flex-col md:grid md:grid-cols-4 gap-4 px-10">
       {allTmdb.map((item) => (
-        <section key={item.id}>
+        <Link to={`/detail/${item.id}`} key={item.id}>
           <img
             src={`https://image.tmdb.org/t/p/w400${item.poster_path}`}
             alt={item.original_title}
@@ -50,16 +55,16 @@ const FetchMovies = () => {
           />
           <p className="text-xl font-medium py-2">{item.original_title}</p>
           <div className="flex gap-2 flex-wrap">
-            {getMovieGenres(item.genre_ids).map((gr) => (
+            {getMovieGenres(item.genre_ids).map((gnr) => (
               <span
-                key={gr.id}
+                key={gnr.id}
                 className="bg-slate-200 text-slate-500 px-3 py-1 rounded-full text-sm"
               >
-                {gr.name}
+                {gnr.name}
               </span>
             ))}
           </div>
-        </section>
+        </Link>
       ))}
     </section>
   );
